475e18a15982d4a85484aff9cc7a3bf0d6645479
jowel007 <abuhanifjowel1@gmail.com>
2024-05-15 17:26:30 +0600

    admin setup - mastering admin login template - I

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -10,9 +10,6 @@
     <link rel="stylesheet" href="{{ asset('admin') }}/assets/modules/fontawesome/css/all.min.css">
 
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="{{ asset('admin') }}/assets/modules/jqvmap/dist/jqvmap.min.css">
-    <link rel="stylesheet" href="{{ asset('admin') }}/assets/modules/weather-icon/css/weather-icons.min.css">
-    <link rel="stylesheet" href="{{ asset('admin') }}/assets/modules/weather-icon/css/weather-icons-wind.min.css">
     <link rel="stylesheet" href="{{ asset('admin') }}/assets/modules/summernote/summernote-bs4.css">
 
     <!-- Template CSS -->
@@ -56,14 +53,9 @@
 <script src="{{ asset('admin') }}/assets/modules/tooltip.js"></script>
 <script src="{{ asset('admin') }}/assets/modules/bootstrap/js/bootstrap.min.js"></script>
 <script src="{{ asset('admin') }}/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-<script src="{{ asset('admin') }}/assets/modules/moment.min.js"></script>
 <script src="{{ asset('admin') }}/assets/js/stisla.js"></script>
 
 <!-- JS Libraies -->
-<script src="{{ asset('admin') }}/assets/modules/simple-weather/jquery.simpleWeather.min.js"></script>
-<script src="{{ asset('admin') }}/assets/modules/chart.min.js"></script>
-<script src="{{ asset('admin') }}/assets/modules/jqvmap/dist/jquery.vmap.min.js"></script>
-<script src="{{ asset('admin') }}/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
 <script src="{{ asset('admin') }}/assets/modules/summernote/summernote-bs4.js"></script>
 <script src="{{ asset('admin') }}/assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
 

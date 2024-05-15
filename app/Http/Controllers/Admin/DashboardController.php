e69fa051f4ec3d1debdcca9f38af11d993d6fd3b
jowel007 <abuhanifjowel1@gmail.com>
2024-05-15 17:05:09 +0600

    admin setup - mastering admin template - II

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-//        return view('admin.dashboard.index');
-        return view('admin.layouts.master');
+        return view('admin.dashboard.index');
+//        return view('admin.layouts.master');
     }
 }

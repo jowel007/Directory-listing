dcd834d016fdb54e178e909dbd05f238142e49c5
jowel007 <abuhanifjowel1@gmail.com>
2024-05-16 11:27:44 +0600

    frontend setup - mastering frontend template - I

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[FrontendController::class,'Index']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');

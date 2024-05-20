a7bcdfd72acf9a9209564a9eb9f8b6e05f1f3412
jowel007 <abuhanifjowel1@gmail.com>
2024-05-20 13:07:06 +0600

    frontend profile - mastering user dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Frontend\DashboardController;
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -21,14 +22,18 @@ Route::get('/',[FrontendController::class,'Index']);
 //     return view('welcome');
 // });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::group(['middleware'=>'auth','prefix'=> 'user','as'=> 'user.'], function(){
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');  // user.dashboard
 });
 
 require __DIR__.'/auth.php';

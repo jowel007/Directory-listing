25a2fd4205cc54dad041d51f95ea15a25c560256
jowel007 <abuhanifjowel1@gmail.com>
2024-05-22 21:46:07 +0600

    home section - home section dynamic data showing in frontend

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hero;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
     public function Index(){
         // return view('frontend.layouts.master');
-        return view('Frontend.home.index');
+        $hero = Hero::first();
+        return view('Frontend.home.index',compact('hero'));
     }
 }

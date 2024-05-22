5e1b8620c32a64623a41b11713b4fd4898393d2d
jowel007 <abuhanifjowel1@gmail.com>
2024-05-22 23:15:56 +0600

    breadcrumb,sidebar activation help of php helper function

---
+++ app/Helpers/GlobalHelper.php
@@ -0,0 +1,16 @@
+
+<?php
+
+/** set sidebar active */
+
+if(!function_exists('setSidebarActive')){
+    function setSidebarActive(array $routes) : ?string
+    {
+        foreach($routes as $route){
+            if(request()->routeIs($route)){
+                return 'active';
+            }
+        }
+        return null;
+    }
+}
